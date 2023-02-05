b34e7ce79718f09d2c29d467c9f638b5d484309a
jinoprinx <jino4rex@gmail.com>
2023-02-05 08:07:26 +0100

    added more starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Form from './components/form';
+//import ChatContainer from './components/chat-box'
+import './app.css';
+import bot from './bot.svg';
+import user from './user.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props){
+  const [display, setDisplay]= useState('')
+  function setRender(resp){
+    setDisplay(resp)
+  }
+ 
+  return(
+    <div id="app">
+      <h1>Jinonet ChatBot</h1>
+      <div>
+        < Form
+        addResponse={setRender} />
+      </div>
     </div>
-  );
+    )
 }
-
 export default App;
